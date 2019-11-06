61bc3d58a3f27fd72d1a35882bf55513a79ae46a
Muhamad Risman <real.risman@gmail.com>
2019-11-06 21:46:32 +0700

    Remove nav from home and redisgn to base design

--- pages/index.js
+++ pages/index.js
@@ -1,38 +1,35 @@
 import React from 'react'
 import Head from 'next/head'
-import Nav from '../components/nav'
 
 const Home = () => (
   <div>
     <Head>
-      <title>Home</title>
-      <link rel='icon' href='/favicon.ico' />
+      <title>My Wishlist</title>
+      <link rel="icon" href="/favicon.ico" />
     </Head>
 
-    <Nav />
+    <div className="hero">
+      <h1 className="title">My Wishlist</h1>
 
-    <div className='hero'>
-      <h1 className='title'>Welcome to Next.js!</h1>
-      <p className='description'>
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className='row'>
-        <a href='https://nextjs.org/docs' className='card'>
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href='https://nextjs.org/learn' className='card'>
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href='https://github.com/zeit/next.js/tree/master/examples'
-          className='card'
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
+      <div className="row">
+        <div className="card">
+          <img src="https://s.catch.com.au/images/product/0025/25766/5d96f32ccba35108889336_w300.jpg" />
+          <p>
+            Razor X Cruiser Electric Skateboard with Wireless 2.4 GHz Remote -
+            Black
+          </p>
+          <button className="btn">ADD TO CART</button>
+        </div>
+        <div className="card">
+          <img src="https://s.catch.com.au/images/product/0002/2012/58a4e4993be38264924111_w300.jpg" />
+          <p>Birkenstock Arizona Unisex Regular Fit Sandals - Dark Brown</p>
+          <button className="btn">ADD TO CART</button>
+        </div>
+        <div className="card">
+          <img src="https://s.catch.com.au/images/product/0025/25826/5d9ee68978bf0654691833_w300.jpg" />
+          <p>Paws & Claws Catsby Double Platform Hideaway Tower - Cream</p>
+          <button className="btn">ADD TO CART</button>
+        </div>
       </div>
     </div>
 
@@ -40,6 +37,12 @@ const Home = () => (
       .hero {
         width: 100%;
         color: #333;
+        font-size: 14px;
+        font-family: Trueno, Helvetica, Arial, sans-serif;
+        font-weight: 100;
+        line-height: 1.4;
+        -webkit-font-smoothing: antialiased;
+        text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.004);
       }
       .title {
         margin: 0;
@@ -62,7 +65,7 @@ const Home = () => (
       .card {
         padding: 18px 18px 24px;
         width: 220px;
-        text-align: left;
+        text-align: center;
         text-decoration: none;
         color: #434343;
         border: 1px solid #9b9b9b;
@@ -75,11 +78,21 @@ const Home = () => (
         color: #067df7;
         font-size: 18px;
       }
+      .card img {
+        width: 100%;
+      }
       .card p {
-        margin: 0;
-        padding: 12px 0 0;
+        margin: 20px 0 10px 0;
         font-size: 13px;
         color: #333;
+      }
+      .card button {
+        background: #28a528;
+        border-radius: 4px;
+        color: #fff;
+        text-transform: uppercase;
+        padding: 12px;
+        width: 100%;
       }
     `}</style>
   </div>
